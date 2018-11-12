c8c7ca7fd328b5e7dcf7c5930b0c6b5823e534b6
Tsys Artem <19successful91@gmail.com>
2018-11-12 10:11:00 +0200

    fix z-index block

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,3 @@
-
-
 function init() {
   $('.section__home').onepage_scroll({
       sectionContainer: ".sl",     // sectionContainer accepts any kind of selector in case you don't want to use section
@@ -17,6 +15,27 @@ function init() {
   $('.ecology__button > input').click( function () {
     tabs();
   });
+
+  AOS.init({
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 20, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  });
+
 
   tabs();
   slickSetting();
@@ -51,6 +70,9 @@ function slickSetting() {
   });
 }
 
-init();
+$(document).ready(function () {
+  init();
+});
+
 
 
